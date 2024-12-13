b0f6ea7bc0c5ffcac191ae4bea20d5114a458bbe
daniel.rajcan <daniel.rajcan.ext@pluxeegroup.com>
2024-12-13 10:56:05 +0100

    Add outputs to main module

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,20 @@
 # --- azure/outputs.tf ---
 
+output "virtual_network" {
+  value = [for x in module.virtual_network[*] : x]
+}
+
 output "vm_public_ip_address" {
   value = azurerm_public_ip.main.ip_address
+}
+output "vm_admin_username" {
+  value = module.virtual_machine.vm_admin_username
+}
+output "vm_admin_password" {
+  sensitive = true
+  value     = module.virtual_machine.vm_admin_password
+}
+
+output "vm_ssh_command" {
+  value = "ssh ${module.virtual_machine.vm_admin_username}@${azurerm_public_ip.main.ip_address}"
 }
